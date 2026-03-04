d6af910616b1c3875a4a107a82d08824fe6249d9
mauglikir <maugli_kir@mail.ru>
2026-03-04 18:20:04 +0300

    new

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+    <nav>
+        <div class="logo__img">
+            <img src="/img/bg.png" alt="" srcset="">
+        </div>
+    </nav>
+</template>
+<style></style>
+<script>
+export default{
+    data() {
+        return {
+            
+        }
+    },
+}
+
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+<template>
+<div>
+    <h2>{{ txt }}</h2>
+</div>
+</template>
+<style>
+</style>
+<script>
+export default{
+    data() {
+        return {
+            txt:'Apart hotel'
+        }
+    },
+}
+
+</script>
